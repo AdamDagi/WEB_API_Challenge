bcf6e6367cd3c11d0691837bc51a988746368331
AdamDagi <mugutdinovshiha02@gmail.com>
2021-10-19 19:00:42 -0400

    wrote html tegs and styled them with  css. Also wrote js function for timer

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const timer = document.querySelector(".timer");
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+        display.style.fontSize = "15px";
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 1;
+    startTimer(fiveMinutes, timer);
+};
